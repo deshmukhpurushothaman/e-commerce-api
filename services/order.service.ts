1b906eb8d8ecd439f3d4c32210541cbe52b5440c
Deshmukh P <deshmukhpurushothaman@gmail.com>
2023-01-11 06:45:54 +0530

    🎁order services added

--- services/order.service.ts
+++ services/order.service.ts
@@ -15,3 +15,29 @@ import { CatalogDocument, CatalogModel } from '../models/catalog.model'
 import { OrdersDocument, OrdersModel } from '../models/orders.model'
 import { DocumentDefinition, FilterQuery } from 'mongoose';
 import { logger } from '../utils/logger/loggerUtil';
+
+export const createOrder = async (
+    input: DocumentDefinition<
+        Omit<
+            OrdersDocument,
+            'updatedAt' | 'createdAt'
+        >
+    >,
+) => {
+    try {
+        const order = new OrdersModel(input);
+        await order.save();
+        return true;
+    } catch (error) {
+        logger.error(error)
+        return false
+    }
+}
+
+export const fetchSellerOrders = async (query: any, populate: string[] | string) => {
+    try {
+        return await OrdersModel.find(query).populate(populate)
+    } catch (error) {
+        throw error
+    }
+}
